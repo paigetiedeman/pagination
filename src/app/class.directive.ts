d82428fae702fd153f8b6181bcb1a5d266b17dea
Paige Tiedeman <paigetiedeman@gmail.com>
2022-01-26 12:55:29 -0800

    styling and adding custom directives

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
+//Attribute Directive
 @Directive({
   selector: '[appClass]'
 })
 export class ClassDirective {
 
-  constructor() { }
+  constructor(private element: ElementRef) {
+    
+  }
 
+  @Input('appClass') set classNames(classObj: any) {
+    for (let key in classObj) {
+      if(classObj[key]) {
+        this.element.nativeElement.classList.add(key);
+      } else {
+        this.element.nativeElement.classList.remove(key);
+      }
+    }
+  }
 }
